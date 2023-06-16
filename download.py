265afc6541d9c7277330ba5e5a8e2959c4c56d2c
root <root@v2202204174534188530.hotsrv.de>
2023-06-16 15:19:06 +0800

    feat: 移除封面,修改下载地址

--- download.py
+++ download.py
@@ -17,13 +17,13 @@ from selenium.webdriver.chrome.options import Options
 
 def download(url):
   encode = 0 #不轉檔
-  action = input('要轉檔嗎?(y/n)')
-  if action.lower() == 'y':
-    action = input('要用GPU(Nvidia)加速轉檔嗎?(y/n)')
-    if action.lower() == 'y':
-       encode = 1 #GPU轉檔
-    else:
-       encode = 2 #CPU轉檔
+#   action = input('要轉檔嗎?(y/n)')
+#   if action.lower() == 'y':
+#     action = input('要用GPU(Nvidia)加速轉檔嗎?(y/n)')
+#     if action.lower() == 'y':
+#        encode = 1 #GPU轉檔
+#     else:
+#        encode = 2 #CPU轉檔
 
   print('正在下載影片: ' + url)
   # 建立番號資料夾
